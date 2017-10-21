9389fb4519c40938db49694cf387832946aff255
Simon Fong <simonfong6@gmail.com>
2017-10-21 03:18:49 -0700

    Defined image capture function

--- capture-image.py
+++ capture-image.py
@@ -1,17 +1,19 @@
 import cv2
 
-cv2.namedWindow("preview")
-vc = cv2.VideoCapture(4)
 
-if vc.isOpened(): # try to get the first frame
-    rval, frame = vc.read()
-else:
-    rval = False
 
-while rval:
-    cv2.imshow("preview", frame)
-    rval, frame = vc.read()
-    key = cv2.waitKey(20)
-    if key == 27: # exit on ESC
-        break
-cv2.destroyWindow("preview")
+def takePicture(name):
+	vc = cv2.VideoCapture(0)
+	if vc.isOpened(): # try to get the first frame
+		rval, frame = vc.read()
+		cv2.imwrite(name,frame)
+		print("Image captured into " + name)
+	else:
+		rval = False
+		print("Image failed to capture.")
+
+def main():
+	takePicture("one.jpg")
+
+if __name__ == '__main__':
+	main()
