ec7418d1577b86b73a37edfa9ad73f225da4e375
Simon Fong <simonfong6@gmail.com>
2017-10-21 15:03:55 -0700

    multimage-saving added

--- capture_image.py
+++ capture_image.py
@@ -1,4 +1,6 @@
 import cv2
+import sys
+import os
 
 def takePicture(name=None, capture_id=0):
     vc = cv2.VideoCapture(capture_id)
@@ -13,7 +15,16 @@ def takePicture(name=None, capture_id=0):
 
 def main():
     # takePicture(name="one.jpg")
-    takePicture(capture_id=6)
+	dirname = 'raw_data' 
+
+	capture_id = int(sys.argv[1])
+	base_name = str(sys.argv[2])
+	num_shots = int(sys.argv[3])
+	
+	for i in range(num_shots):
+		name = base_name + '_' + str(i) + '.jpg'
+		takePicture(os.path.join(dirname,base_name,name),capture_id=capture_id)
+		
 
 if __name__ == '__main__':
     main()
