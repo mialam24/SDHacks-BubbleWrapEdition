f791536e1353b6c2967117ef3908587dd644a8f6
Ismail Alam <ismail.alam2@gmail.com>
2017-10-21 09:38:21 -0700

    now have image scraping script

--- capture-image.py
+++ capture-image.py
@@ -1,19 +1,17 @@
 import cv2
 
-
-
-def takePicture(name):
-	vc = cv2.VideoCapture(0)
+def takePicture(name, capture_id=0):
+	vc = cv2.VideoCapture(capture_id)
 	if vc.isOpened(): # try to get the first frame
-		rval, frame = vc.read()
+		_, frame = vc.read()
 		cv2.imwrite(name,frame)
 		print("Image captured into " + name)
 	else:
-		rval = False
 		print("Image failed to capture.")
 
 def main():
-	takePicture("one.jpg")
+	# takePicture("one.jpg")
+	takePicture("one.jpg", 6)
 
 if __name__ == '__main__':
 	main()
