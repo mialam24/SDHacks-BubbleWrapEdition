df130ea13675123b33fa52b76f65937676bba954
Simon Fong <simonfong6@gmail.com>
2017-10-21 18:03:06 -0700

    Added failed file

--- capture_image.py
+++ capture_image.py
@@ -1,6 +1,7 @@
 import cv2
 import sys
 import os
+import time
 
 def takePicture(name=None, capture_id=0):
     vc = cv2.VideoCapture(capture_id)
@@ -20,10 +21,12 @@ def main():
 	capture_id = int(sys.argv[1])
 	base_name = str(sys.argv[2])
 	num_shots = int(sys.argv[3])
+	fps = float(sys.argv[4])
 	
 	for i in range(num_shots):
 		name = base_name + '_' + str(i) + '.jpg'
 		takePicture(os.path.join(dirname,base_name,name),capture_id=capture_id)
+		time.sleep(0.03)
 		
 
 if __name__ == '__main__':
